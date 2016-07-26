3be648ceb22d1818a0a820682d09d8ee0c22cf21
smart-coding <losts721@qq.com>
2016-07-26 19:27:27 +0800

    初始化工程

---
+++ springboot-web/src/main/java/com/smart/web/Application.java
@@ -0,0 +1,16 @@
+package com.smart.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 程序主入口
+ * 
+ * @author lenovo
+ *
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
