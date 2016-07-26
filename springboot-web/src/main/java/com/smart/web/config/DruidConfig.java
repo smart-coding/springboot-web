40f52da7c698cf6dd723aba1b7662b32dea03e81
smart-coding <losts721@qq.com>
2016-07-26 20:54:28 +0800

    添加druid配置及log4j2配置

---
+++ springboot-web/src/main/java/com/smart/web/config/DruidConfig.java
@@ -0,0 +1,55 @@
+package com.smart.web.config;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+/**
+ * druid数据源注入
+ * 
+ * @author smart-coding
+ *
+ */
+@Configuration
+public class DruidConfig {
+	
+	@Bean
+    public ServletRegistrationBean druidServlet() {
+        return new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+    }
+
+    @Bean
+    public DataSource druidDataSource(@Value("${spring.datasource.driverClassName}") String driver,
+                                      @Value("${spring.datasource.url}") String url,
+                                      @Value("${spring.datasource.username}") String username,
+                                      @Value("${spring.datasource.password}") String password) {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setDriverClassName(driver);
+        druidDataSource.setUrl(url);
+        druidDataSource.setUsername(username);
+        druidDataSource.setPassword(password);
+        try {
+            druidDataSource.setFilters("stat, wall");
+        } catch (SQLException e) {
+        }
+        return druidDataSource;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+}
